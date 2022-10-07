100eb0a98be0b65607421ad0bfafc9bc63513796
ivan886 <ivanfernando.fonseca@gmail.com>
2022-10-06 23:55:21 -0500

    FIX se corrige el lugar donde se realizara el despliegue

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://ivan886.github.io/',   //se indica donde se alojara el sitio
+  base: 'https://ivan886.github.io/react-site/',   //se indica donde se alojara el sitio
   plugins: [react()]
 })
